7e704c72e3681ac4247d9b2a03e15d749af79195
Tim Clifford <tim.clifford@amazee.com>
2021-10-19 17:27:55 +0100

    statis 404 page

---
+++ services/ui/src/pages/404.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import StatusLayout from 'layouts/StatusLayout';
+
+// Statically generated 404 page
+export default function FourOhFour() {
+  return <>
+    <StatusLayout>
+      <Head>
+        <title>404 Page Not Found | Lagoon</title>
+      </Head>
+      <h1>404 - Page Not Found</h1>
+         <Link href="/projects">
+      <a>
+        Go back home
+      </a>
+    </Link>
+    </StatusLayout>
+  </>
+}
