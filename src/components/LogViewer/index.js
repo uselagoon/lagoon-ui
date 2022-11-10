a88ef1b55fcdc6796f45acc52ec00b6637f33656
Blaize Kaye <blaize.kaye@amazee.com>
2022-11-10 15:53:55 +1300

    Removes commented out code

--- src/components/LogViewer/index.js
+++ src/components/LogViewer/index.js
@@ -4,17 +4,9 @@ import LogAccordion from 'components/LogViewer/LogAccordion';
 import withState from 'recompose/withState';
 import withHandlers from 'recompose/withHandlers';
 
-
-// const withParseLogsState = withState("checkedParseState", "setParseStateChecked", true);
-
-// const withParseLogsStateHandlers = withHandlers({
-//   changeState: ({setParseStateChecked, checkedParseState}) => (e) => {setParseStateChecked(!checkedParseState);},
-// })
-
 const LogViewer = ({ logs, status = "NA", checkedParseState, changeState }) => (
   <React.Fragment>
     <div className="logs">
-    {/* <div className="parseLogs"><input type="checkbox" checked={ checkedParseState } onChange={changeState}></input><span className="showraw">Prettify logs</span></div> */}
     { logs !== null ?
         checkedParseState ?
         (<div className="log-viewer">{logPreprocessor(logs, status)}</div>)
