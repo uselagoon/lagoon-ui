dca43bb5323caa3abbcb7286ceb02f925ac4b4d4
Davit <davedabyeet@gmail.com>
2023-04-05 14:38:27 +0400

    duplicate imports

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,8 +11,6 @@ import { useTourContext } from "../tours/TourContext";
 
 import { useTranslation } from "react-i18next";
 import i18next from "i18next";
-import QueryError from "../components/errors/QueryError";
-import { useTourContext } from "../tours/TourContext";
 
 /**
  * Displays the projects page.
