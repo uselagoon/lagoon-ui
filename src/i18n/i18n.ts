f66faaf4c710d1ca5925ad5a17973b9b2406d459
Davit <davedabyeet@gmail.com>
2023-04-05 14:05:53 +0400

    build issue

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
-import languageDetector from "i18next-browser-languageDetector";
+import languageDetector from "i18next-browser-languagedetector";
 import { dictionary } from "./dictionary";
 
 
