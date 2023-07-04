9c5686f4638831095596438111cffade57ee09fc
Tim Clifford <tim.clifford@amazee.com>
2023-07-04 11:22:11 +0100

    workaround for MostProjects query after api changes broke things

--- services/ui/src/lib/query/MostActiveProjects.js
+++ services/ui/src/lib/query/MostActiveProjects.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export default gql`
-  query getProjects($limit: Int, $skip: Int) {
-    uiProjects(limit: $limit, skip: $skip) {
+  {
+    allProjects {
       id
       name
       gitUrl
