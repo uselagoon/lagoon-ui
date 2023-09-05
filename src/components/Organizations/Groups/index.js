516ae4c761d22050c0abd312e82cd7b1ab5afd4f
Davit <davedabyeet@gmail.com>
2023-09-05 10:27:52 +0400

    member count

--- src/components/Organizations/Groups/index.js
+++ src/components/Organizations/Groups/index.js
@@ -86,7 +86,7 @@ const Groups = ({ groups = [], organizationId, organizationName, ableToAddGroup,
       width: '15%',
       key: 'members',
       render: i => {
-        return i.memberCount && <span>Members: {i.memberCount} </span>;
+        return typeof i.memberCount !== 'undefined' && <span>Members: {i.memberCount} </span>;
       },
     },
     {
--- src/pages/organizations/project.js
+++ src/pages/organizations/project.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import Head from 'next/head';
 import { withRouter } from 'next/router';
