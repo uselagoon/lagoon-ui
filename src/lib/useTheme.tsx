7ae8270cbfd99f52b6adf6f5798b3a04163ed93d
Davit <davedabyeet@gmail.com>
2023-09-12 20:09:59 +0400

    default to light theme

--- src/lib/useTheme.tsx
+++ src/lib/useTheme.tsx
@@ -5,12 +5,18 @@ const useTheme = () => {
 
   useEffect(() => {
     const storageTheme = localStorage.getItem('theme');
+    // already previously set in browser store.
     if (storageTheme && ['light', 'dark'].includes(storageTheme)) {
       setTheme(storageTheme);
     } else {
+      // try to automatically infer dark mode theme.
       if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         setTheme('dark');
         localStorage.setItem('theme', 'dark');
+      } else {
+        // default to light
+        setTheme('light');
+        localStorage.setItem('theme', 'light');
       }
     }
   }, []);
