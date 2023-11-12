a8bd5c961c7e324e57d9ae7daf60709ed844282e
cgoodwin90 <chris_2190@hotmail.com>
2023-11-13 10:01:26 +1100

    Various changes to deploy targets page

--- src/components/DeployTargets/StyledDeployTargets.tsx
+++ src/components/DeployTargets/StyledDeployTargets.tsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import { bp, color } from "lib/variables";
 
 export const DeployTargetWrapper = styled.div`
-  text-align: center;
   width: 50%;
   margin: 10px auto;
 
@@ -14,6 +13,7 @@ export const DeployTargetWrapper = styled.div`
   label.dt-branch,
   div.dt-branch {
     flex: 1.8;
+    font-family: monospace;
   }
 
   label.dt-pr,
@@ -41,7 +41,6 @@ export const DeployTargetWrapper = styled.div`
 
     @media ${bp.wideUp} {
       align-items: center;
-      display: flex;
       margin: 0 0 14px;
     }
     @media ${bp.smallOnly} {
@@ -52,7 +51,6 @@ export const DeployTargetWrapper = styled.div`
     }
 
     label {
-      display: none;
       padding-left: 20px;
       @media ${bp.wideUp} {
         display: block;
@@ -80,14 +78,11 @@ export const DeployTargetWrapper = styled.div`
       border-radius: 0;
       line-height: 2rem;
       padding: 8px 0 7px 0;
-
+      display: flex;
+      justify-content: space-between
       > div {
         word-wrap: break-word;
         word-break: break-all;
-      }
-      @media ${bp.wideUp} {
-        display: flex;
-        justify-content: space-between;
       }
 
       & > div {
