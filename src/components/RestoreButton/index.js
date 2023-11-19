81818e3b0240a2d2ef54ff0ac673264d2fc9f8fc
shreddedbacon <b@benjackson.email>
2023-11-20 10:46:28 +1100

    feat: add restoresize to download button

--- src/components/RestoreButton/index.js
+++ src/components/RestoreButton/index.js
@@ -3,6 +3,12 @@ import React from 'react';
 import Button from 'components/Button';
 import Prepare from 'components/RestoreButton/Prepare';
 
+
+function humanFileSize(size) {
+  var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+  return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+}
+
 /**
  * A button to restore a backup.
  */
@@ -13,7 +19,7 @@ const RestoreButton = ({ backup: { backupId, restore } }) => {
 
   if (restore.status === 'failed') return <Button disabled>Retrieve failed</Button>;
 
-  return <Button href={restore.restoreLocation}>Download</Button>;
+  return <Button href={restore.restoreLocation}>Download ({humanFileSize(restore.restoreSize)})</Button>;
 };
 
 export default RestoreButton;
