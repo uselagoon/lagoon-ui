71e30ed6cc2361408ad5732b84fc54349583b8b0
Ben Jackson <b@benjackson.email>
2023-11-25 14:00:33 +1100

    feat: support setting the idp hint as an envvar

--- src/next.config.js
+++ src/next.config.js
@@ -16,6 +16,7 @@ module.exports = {
     GRAPHQL_API_TOKEN: process.env.GRAPHQL_API_TOKEN,
     GRAPHQL_API: process.env.GRAPHQL_API,
     KEYCLOAK_API: process.env.KEYCLOAK_API,
+    KEYCLOAK_IDPHINT: process.env.KEYCLOAK_IDPHINT,
     LAGOON_UI_ICON: process.env.LAGOON_UI_ICON,
     LAGOON_UI_TASK_BLOCKLIST: taskBlocklist,
     LAGOON_VERSION: process.env.LAGOON_VERSION,
