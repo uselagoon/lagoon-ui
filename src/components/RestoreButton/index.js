2644d4428931400f4f8c18c53e8f511ffb3dc8cf
Davit <davedabyeet@gmail.com>
2023-11-29 16:42:56 +0400

    download text formatting

--- src/components/RestoreButton/index.js
+++ src/components/RestoreButton/index.js
@@ -3,10 +3,11 @@ import React from 'react';
 import Button from 'components/Button';
 import Prepare from 'components/RestoreButton/Prepare';
 
-
 function humanFileSize(size) {
   var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
-  return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+  const formatted = (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+
+  return [formatted.length > 5, formatted];
 }
 
 /**
@@ -19,7 +20,13 @@ const RestoreButton = ({ backup: { backupId, restore } }) => {
 
   if (restore.status === 'failed') return <Button disabled>Retrieve failed</Button>;
 
-  return <Button href={restore.restoreLocation}>Download ({humanFileSize(restore.restoreSize)})</Button>;
+  const [isOverflowing, formattedSize] = humanFileSize(restore.restoreSize);
+
+  return (
+    <Button variant={`download ${isOverflowing ? 'btn-download-lg' : ''}`} href={restore.restoreLocation}>
+      Download ({formattedSize})
+    </Button>
+  );
 };
 
 export default RestoreButton;
