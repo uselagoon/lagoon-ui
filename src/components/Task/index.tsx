d1f08ba7c643edce269a1ff3432f6f66cb04542f
cgoodwin90 <chris_2190@hotmail.com>
2023-12-27 16:10:05 +1100

    Enables parsed logs for tasks

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -79,7 +79,7 @@ const Task: FC<TaskProps> = ({ task, projectId, environmentId }) => {
           </div>
         )}
       </div>
-      <LogViewer logs={task.logs} status={task.status} changeState={null} checkedParseState={null} />
+      <LogViewer logs={task.logs} status={task.status} changeState={null} checkedParseState={true} forceLastSectionOpen={true} />
     </StyledTask>
   );
 };
