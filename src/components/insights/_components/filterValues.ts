9df45294ca16c7cee25f42e85b75bdbd49ab29a0
Davit <davedabyeet@gmail.com>
2024-11-11 02:09:26 +0400

    insights fully functional

---
+++ src/components/insights/_components/filterValues.ts
@@ -0,0 +1,108 @@
+export const resultsFilterValues = [
+  {
+    label: '10 results per page',
+    value: 10,
+  },
+  {
+    label: '25 results per page',
+    value: 25,
+  },
+  {
+    label: '50 results per page',
+    value: 50,
+  },
+  {
+    label: '100 results per page',
+    value: 100,
+  },
+  {
+    label: 'All',
+    value: -1,
+  },
+];
+
+export const factsSortvalues = [
+  {
+    value: null,
+    label: 'Sort by',
+  },
+  {
+    value: 'name_asc',
+    label: 'Name asc',
+  },
+  {
+    value: 'name_desc',
+    label: 'Name desc',
+  },
+  {
+    value: 'description_asc',
+    label: 'Description asc',
+  },
+  {
+    value: 'description_desc',
+    label: 'Description desc',
+  },
+  {
+    value: 'source_asc',
+    label: 'Source asc',
+  },
+  {
+    value: 'source_desc',
+    label: 'Source desc',
+  },
+  {
+    value: 'value_asc',
+    label: 'Value asc',
+  },
+  {
+    value: 'value_desc',
+    label: 'Value desc',
+  },
+];
+
+export const insightsSortValues = [
+  {
+    value: null,
+    label: 'Sort by',
+  },
+  {
+    value: 'name_asc',
+    label: 'Name asc',
+  },
+  {
+    value: 'name_desc',
+    label: 'Name desc',
+  },
+  {
+    value: 'service_asc',
+    label: 'Service asc',
+  },
+  {
+    value: 'service_desc',
+    label: 'Service desc',
+  },
+  {
+    value: 'type_asc',
+    label: 'Type asc',
+  },
+  {
+    value: 'type_desc',
+    label: 'Type desc',
+  },
+  {
+    value: 'created_asc',
+    label: 'Created asc',
+  },
+  {
+    value: 'created_desc',
+    label: 'Created desc',
+  },
+  {
+    value: 'size_asc',
+    label: 'Size asc',
+  },
+  {
+    value: 'size_desc',
+    label: 'Size desc',
+  },
+];
